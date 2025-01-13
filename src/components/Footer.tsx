8d6748f29e0eee14ab96ffbb08504ebcb4c4c727
abhish8bits <9bitsmonk@gmai.com>
2025-01-13 17:29:07 +0530

    fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,4 @@
-import { FaInstagram } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa6";
 import { IoMdMailUnread } from "react-icons/io";
 import { FaPhoneAlt } from "react-icons/fa";
 import logo from "../assets/logo.png";
